521f4b7b100220a77755964de89391399db5539f
MAJDER0 <tomasz.majder89@gmail.com>
2023-10-11 23:39:56 +0200

    Caesar Cipher

---
+++ Ciphers/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Ciphers.Models;
+
+namespace Ciphers.Controllers
+{
+    public class HomeController : Controller 
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Caesar()
+        {
+            return View();
+        }
+        // Other actions
+    }
+}
---
+++ Ciphers/wwwroot/js/site.js
@@ -0,0 +1,45 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+let encryptkey = 0;
+let decryptkey = 0;
+
+var Alphabet = {
+    1: 'A',
+    2: 'Ą',
+    3: 'B',
+    4: 'C',
+    5: 'Ć',
+    6: 'D',
+    7: 'E',
+    8: 'Ę',
+    9: 'F',
+    10: 'G',
+    11: 'H',
+    12: 'I',
+    13: 'J',
+    14: 'K',
+    15: 'L',
+    16: 'Ł',
+    17: 'M',
+    18: 'N',
+    19: 'Ń',
+    20: 'O',
+    21: 'Ó',
+    22: 'P',
+    23: 'Q',
+    24: 'R',
+    25: 'S',
+    26: 'Ś',
+    27: 'T',
+    28: 'U',
+    29: 'V',
+    30: 'W',
+    31: 'X',
+    32: 'Y',
+    33: 'Z',
+    34: 'Ź',
+    35: 'Ż'
+};
